1be67abd6c0da06043b025f1fae487d9687a9a49
afr2903 <afr102903@gmail.com>
2019-03-08 17:39:36 -0600

    .

--- Out-of-bounds.ino
+++ Out-of-bounds.ino
@@ -1,19 +1,21 @@
 #include<EEPROM.h>
 #include "I2Cdev.h"
 #include "HMC5883L.h"
-#include "HTInfraredSeeker.h"
+#include <HTInfraredSeeker.h>
 HMC5883L mag;
 
 int16_t mx, my, mz;
-int dir=0;
-float a,b,c;
+int dir=0, last_pos=0;
+float a,b,c, x;
+int r, h=15;
 bool st;
-float deg=0, pk=2, ps=.8;
+float deg=0, pk=.75, ps=.3;
 float zero=0.00f, current;
-bool line_detected;
-unsigned long long begin_time;
 
-int ir[]={150,180,210,225,240,0,30,120,150,180};
+bool line_detected;
+long long begin_time=0;
+
+int ir[]={150,180,210,225,310,0,40,120,160,210};
 int line[10];
 /*
 A---B
@@ -25,12 +27,12 @@ void reset(float heading){
   delay(500);
 }
 void turnoff(){
-  analogWrite(11,0);
-  analogWrite(10,0);
-  analogWrite(7,0);
+  analogWrite(12,0);
+  analogWrite(13,0);
+  analogWrite(2,0);
+  analogWrite(3,0);
   analogWrite(6,0);
-  analogWrite(5,0);
-  analogWrite(4,0);
+  analogWrite(7,0);
 }
 void align(){
   int d=20;
@@ -52,40 +54,41 @@ void align(){
     deg*=2;
     deg+=60;
     deg=min(deg,255);
-    analogWrite(7,st*deg);
-    analogWrite(6,(!st)*deg);
-    analogWrite(5,st*deg);
-    analogWrite(4,(!st)*deg);
-    analogWrite(11,st*deg);
-    analogWrite(10,(!st)*deg);
+    analogWrite(6,st*deg);
+    analogWrite(7,(!st)*deg);
+    analogWrite(2,st*deg);
+    analogWrite(3,(!st)*deg);
+    analogWrite(13,st*deg);
+    analogWrite(12,(!st)*deg);
   }
 }
 void getOut(){
   turnoff();
-  //align();
-  //turnoff();
-  //delay(300);
+  delay(100);
+  align();
+  turnoff();
+  delay(100);
   a=sin((dir-300)*M_PI/180)*180;
   b=sin((dir-60)*M_PI/180)*180;
   c=sin((dir-180)*M_PI/180)*180;
   
   a>0? st=0: st=1;
   a=abs(a);
-  a=min(max(a,5),255);
-  analogWrite(11,st*a);
-  analogWrite(10,(!st)*a);
+  a=min(max(a,0),255);
+  analogWrite(13,st*a);
+  analogWrite(12,(!st)*a);
 
   b>0? st=0: st=1;
   b=abs(b);
-  b=min(max(b,5),255);
-  analogWrite(5,st*b);
-  analogWrite(4,(!st)*b);
+  b=min(max(b,0),255);
+  analogWrite(2,st*b);
+  analogWrite(3,(!st)*b);
   
   c>0? st=0: st=1;
   c=abs(c);
-  c=min(max(c,5),255);
-  analogWrite(7,st*c);
-  analogWrite(6,(!st)*c);
+  c=min(max(c,0),255);
+  analogWrite(6,st*c);
+  analogWrite(7,(!st)*c);
 }
 
 void setup() {
@@ -99,31 +102,14 @@ void setup() {
 
   pinMode(A1,INPUT);
   
-  pinMode(9,OUTPUT);
-  pinMode(8,OUTPUT);
   pinMode(6,OUTPUT);  //A
   pinMode(7,OUTPUT);
-  
-  pinMode(5,OUTPUT);
-  pinMode(4,OUTPUT);
   pinMode(2,OUTPUT);  //B
   pinMode(3,OUTPUT);
-  
   pinMode(13,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(10,OUTPUT); //C
-  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT); //C
+  turnoff();
   
-
-  digitalWrite(9,1);  //A
-  digitalWrite(8,0);
-  
-  digitalWrite(2,0);  //B
-  digitalWrite(3,1);
-  
-  digitalWrite(13,1); //C
-  digitalWrite(12,0);
-
 
   pinMode(A7,INPUT);
   pinMode(A8,INPUT);  //Left
@@ -154,6 +140,7 @@ void setup() {
 
 
 void loop() {
+  
   Serial.print(zero);
   Serial.print('\t');
 
@@ -164,26 +151,32 @@ void loop() {
     Serial.print(line[i]);
     Serial.print('\t');
   }
-  if(line[6]>=110||line[5]>=110||line[4]>=110){
+  
+  if(line[3]>=160||line[2]>=255||line[1]>=210){
+    if(dir<50&&dir>300) dir=180;
+    else dir=90;
+    line_detected=1;
+  }
+  if(line[9]>=125||line[8]>=170||line[7]>=150){
+    if(dir<50&&dir>300) dir=180;
+    else dir=270;
+    line_detected=1;
+  }
+  if((line[9]>=125||line[8]>=170||line[7]>=140)&&(line[3]>=145||line[2]>=250||line[1]>=210)){
+    if(dir<90&&dir>270) dir=180;
+    else dir=0;
+    line_detected=1;
+  }
+  if(line[6]>=210||line[5]>=225||line[4]>=260){
     dir=180;
     line_detected=1;
   }
-  if(line[9]>=100||line[8]>=120||line[7]>=110){
-    dir=270;
-    line_detected=1;
-  }
-  if(line[3]>=100||line[2]>=100||line[1]>=100){
-    dir=90;
-    line_detected=1;
-  }
-
 
   if(line_detected){
     getOut();
     delay(400);
     turnoff();
     delay(100);
-    align();
     turnoff();
     delay(100);
     line_detected=0;
@@ -208,36 +201,45 @@ void loop() {
   Serial.print(deg);
   Serial.print('\t');
   deg*=pk;
-
-  dir= ir[IR.Direction];
   
-  a=sin((dir-300)*M_PI/180)*230;
-  b=sin((dir-60)*M_PI/180)*230;
-  c=sin((dir-180)*M_PI/180)*230;
+  r=abs(IR.Strength-130);
+  x=cos(ir[IR.Direction]*M_PI/180);
+  dir= IR.Direction>5? acos((r*x-h)/r)*(180/M_PI): 360-(acos((r*x-h)/r)*(180/M_PI));
+  if(IR.Direction>7||IR.Direction<3) dir=ir[IR.Direction];
+  if(IR.Direction==5) dir=0;
+  if(IR.Direction==0){
+    if(last_pos<5) dir=140;
+    else dir=220;
+  }
+  else last_pos= IR.Direction;
+    
+  a=sin((dir-300)*M_PI/180)*200;
+  b=sin((dir-60)*M_PI/180)*200;
+  c=sin((dir-180)*M_PI/180)*200;
   
   a>0? st=0: st=1;
   a=abs(a);
   st? a+=deg : a-=deg;
-  a-=IR.Strength*ps;
-  a=min(max(a,5),255);
-  analogWrite(11,st*a);
-  analogWrite(10,(!st)*a);
+  if(IR.Strength<=120)a-=IR.Strength*ps;
+  a=min(max(a,0),255);
+  analogWrite(13,st*a);
+  analogWrite(12,(!st)*a);
 
   b>0? st=0: st=1;
   b=abs(b);
   st? b+=deg : b-=deg;
-  b-=IR.Strength*ps;
-  b=min(max(b,5),255);
-  analogWrite(5,st*b);
-  analogWrite(4,(!st)*b);
+  if(IR.Strength<=120)b-=IR.Strength*ps;
+  b=min(max(b,0),255);
+  analogWrite(2,st*b);
+  analogWrite(3,(!st)*b);
   
   c>0? st=0: st=1;
   c=abs(c);
   st? c+=deg : c-=deg;
-  c-=IR.Strength*ps;
-  c=min(max(c,5),255);
-  analogWrite(7,st*c);
-  analogWrite(6,(!st)*c);
+  if(IR.Strength<=120)c-=IR.Strength*ps;
+  c=min(max(c,0),255);
+  analogWrite(6,st*c);
+  analogWrite(7,(!st)*c);
 
   Serial.print(a);
   Serial.print('\t');
